7b9e436231ea7282bf597114ac1882bdfb8ecc60
LuizaEduardaJung <luiza-ejung@educar.rs.gov.br>
2023-05-11 22:08:20 -0300

    exercicio03

--- Exercicio03/Exercicio03/src/exercicio03/Main.java
+++ Exercicio03/Exercicio03/src/exercicio03/Main.java
@@ -5,8 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        Produto produto1 = new Livro("V. E. Schwab", "A Vida Invisivel de Addie LaRue", 250.23);
-        Produto produto2 = new CD("Lana Del Rey", "Young and Beautiful", 15.23);
+        Produto produto1 = new Livro("V. E. Schwab", "A Vida Invisivel de Addie LaRue", 60.00);
+        Produto produto2 = new CD("Lana Del Rey", "Young and Beautiful", 20.00);
        
         System.out.println(produto1.exibirDetalhes());
         System.out.println(produto2.exibirDetalhes());
